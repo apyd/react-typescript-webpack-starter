690ed2b40bf2cc2fc9bbd66878af822d8b76e1dd
apyd <arturpydych@gmail.com>
2022-05-22 19:44:05 +0200

    npm scripts update + HMR support

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
+const webpack = require('webpack');
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  devServer: {
+    static: './dist',
+    hot: true
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './public/index.html'),
       filename: 'index.html'
     })
   ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
   module: {
     rules: [
       {
@@ -37,5 +37,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
 }
